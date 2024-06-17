13fedf12902bc114d6bb917754c4727175236d21
Hafiz Adewuyi <hoadewuyi@gmail.com>
2024-06-17 12:29:26 +0300

    Create tests for Default Income Tax Calculator

---
+++ Application.UnitTests/TaxCalculators/Default/DefaultIncomeTaxCalculatorTests.cs
@@ -0,0 +1,100 @@
+﻿using Application.TaxCalculators.Default;
+using Core.Configuration;
+using Core.Configuration.Parameters;
+using Core.Deductibles;
+using Core.Dto;
+using Moq;
+
+namespace Application.UnitTests.TaxCalculators.Default
+{
+    [TestFixture]
+    public class DefaultIncomeTaxCalculatorTests
+    {
+        private Mock<IConfigurationFactory> _mockConfigurationFactory;
+        private Mock<IDeductibleFactory> _mockDeductibleFactory;
+        private Mock<IConfigProvider> _mockConfigProvider;
+        private Mock<IDeductibleCalculator> _mockDeductibleCalculator;
+        private BaseTaxParameters _baseTaxParameters;
+        private DefaultIncomeTaxCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockConfigurationFactory = new Mock<IConfigurationFactory>();
+            _mockDeductibleFactory = new Mock<IDeductibleFactory>();
+            _mockConfigProvider = new Mock<IConfigProvider>();
+            _mockDeductibleCalculator = new Mock<IDeductibleCalculator>();
+
+            _baseTaxParameters = new BaseTaxParameters
+            {
+                // Use same parameters as in the example
+                TaxFreeAmount = 1000,
+                Percentage = 10
+            };
+
+            _mockConfigProvider.Setup(cp => cp.Get<BaseTaxParameters>())
+                .Returns(_baseTaxParameters);
+
+            _mockConfigurationFactory.Setup(cf => cf.GetConfigProvider("Default:IncomeTax"))
+                .Returns(_mockConfigProvider.Object);
+
+            _mockDeductibleFactory.Setup(df => df.GetCalculators("Default:IncomeTax"))
+                .Returns(new[] { _mockDeductibleCalculator.Object });
+
+            _calculator = new DefaultIncomeTaxCalculator(
+                _mockConfigurationFactory.Object,
+                _mockDeductibleFactory.Object);
+        }
+
+        [Test]
+        public void TaxFieldName_ShouldReturn_IncomeTax()
+        {
+            Assert.That(_calculator.TaxFieldName, Is.EqualTo("IncomeTax"));
+        }
+
+        [Test]
+        public void UniqueName_ShouldReturn_DefaultIncomeTax()
+        {
+            Assert.That(_calculator.UniqueName, Is.EqualTo("Default:IncomeTax"));
+        }
+
+        [Test]
+        public void IsApplicableTo_ShouldReturnTrue_ForNumericSSN()
+        {
+            var taxPayer = new TaxPayer { SSN = "123456789" };
+            Assert.IsTrue(_calculator.IsApplicableTo(taxPayer));
+        }
+
+        [Test]
+        public void IsApplicableTo_ShouldReturnFalse_ForNonNumericSSN()
+        {
+            var taxPayer = new TaxPayer { SSN = "ABC123456" };
+            Assert.IsFalse(_calculator.IsApplicableTo(taxPayer));
+        }
+
+        [TestCase("Irina", "123456789", 3400, 0, 240)]
+        [TestCase("George", "987654321", 980, 0, 0)]
+        [TestCase("Mick", "321987654", 2500, 150, 135)]
+        [TestCase("Bill", "219876543", 3600, 360, 224)]
+        public void Calculate_ShouldReturnCorrectTaxAmount(string fullName, string ssn, decimal grossIncome, decimal deductible,
+            decimal expectedTax)
+        {
+            var taxPayer = new TaxPayer { FullName = fullName, SSN = ssn, GrossIncome = grossIncome };
+            _mockDeductibleCalculator.Setup(dc => dc.Calculate(taxPayer)).Returns(deductible);
+
+            var result = _calculator.Calculate(taxPayer);
+
+            Assert.That(result, Is.EqualTo(expectedTax));
+        }
+
+        [Test]
+        public void Calculate_ShouldReturnZero_ForNonApplicableTaxPayer()
+        {
+            var taxPayer = new TaxPayer { SSN = "ABC123456", GrossIncome = 50000m };
+
+            var result = _calculator.Calculate(taxPayer);
+
+            Assert.That(result, Is.EqualTo(0m));
+        }
+    }
+}
